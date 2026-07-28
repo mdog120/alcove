f9b83cde76bbec313ea2c67074442ede65f6c689
Rola Omotoso <rolaomotoso@Rolas-MacBook-Air.local>
2026-07-28 11:21:15 -0500

    Correct Supabase project endpoint

--- js/config.js
+++ js/config.js
@@ -3,6 +3,6 @@
    ========================================================================== */
 
 export const SUPABASE_CONFIG = {
-    URL: "https://iuqwxixqryxzgrsywsfwp.supabase.co",
+    URL: "https://iuqwxiqryxzgrsywsfwp.supabase.co",
     ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml1cXd4aXFyeXh6Z3JzeXdzZndwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODUxNTg3MTUsImV4cCI6MjEwMDczNDcxNX0.4fYiBUDAK8eK50yQmiXGSEB4kVF2tBkMXh1CS-Z7mVY"
 };
